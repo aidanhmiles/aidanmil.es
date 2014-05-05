d8cbc71440582c59d923e09542a639432007cfc9
Aidan Miles <itsthejazzkid@gmail.com>
2014-05-04 18:43:45 -0700

    add content for the other slides, fix icons

--- hire-me.js
+++ hire-me.js
@@ -1,11 +1,3 @@
-function resizeBlocks(){
-
-	vpwidth = $(window).width();
-	vpheight = $(window).height();
-
-	$('section').css('min-height', vpheight);
-
-};
 
 $(document).ready(function(){
 
@@ -18,3 +10,28 @@ $(document).ready(function(){
 $(window).on('resize', function(){
 	resizeBlocks();
 });
+
+
+function resizeBlocks(){
+
+	vpwidth = $(window).width();
+	vpheight = $(window).height();
+
+	$('section').css('min-height', vpheight);
+
+};
+
+function isElementInViewport (el) { 
+	// john resig's implementation
+    //special bonus for those using jQuery
+    if (el instanceof jQuery) {
+        el = el[0];
+    } 
+    var rect = el.getBoundingClientRect(); 
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+};
