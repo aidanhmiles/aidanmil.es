c50f99f59c7cc5fc32de5cb9335b03880ccd56c1
Aidan Miles <aidan.miles@piksel.com>
2015-11-06 17:42:50 -0800

    adds js to enable skipping the intro animation

--- js/hire-me.js
+++ js/hire-me.js
@@ -1,3 +1,23 @@
+/* FOR ANIMATION SKIPPING */
+
+// http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var skip = getUrlParameter('skip') === 'true' ? true : false;
+
 $(document).ready(function(){
 
     resizeBlocks(); 
@@ -5,7 +25,7 @@ $(document).ready(function(){
     $('#slide1').css('top', vpheight);
     console.log("loaded");
 
-    if (window.Promise){
+    if (window.Promise && !skip){
 
     // SPINNER STUFF
     Spinner.setup();
