88eaa141224408dea6b6d988a71bbdc3e98a283e
Aidan Miles <amiles@seamgen.com>
2016-07-25 21:56:20 -0700

    fixes deploy script to build, then deploy

--- s3_deploy.sh
+++ s3_deploy.sh
@@ -1,8 +1,14 @@
 #/usr/bin/env bash
 
+./gulp build --production
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-aws --profile aidanmil.es_s3 s3 cp $DIR/client/dist/ s3://aidanmil.es --recursive 
+# First time
+# aws --profile aidanmil.es_s3 s3 cp $DIR/client/dist/ s3://aidanmil.es --recursive 
+
+# Subsequent times
+aws --profile aidanmil.es_s3 s3 sync $DIR/client/dist/ s3://aidanmil.es
 
 # files=($DIR/client/dist/*)
 # echo "${files[@]}"
